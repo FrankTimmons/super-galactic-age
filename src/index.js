3158ec9d2e7c7a897bdc9cacee1a17efe1c22d8b
FrankTimmons <franktimmonspdx@gmail.com>
2022-06-24 15:13:37 -0700

    Add values for form inputs.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import { User } from './js/ageCalculator.js'
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import { User } from './js/ageCalculator.js'
+
+$('form#age-calc-form').submit(function(event){
+  event.preventDefault();
+  let age = $('#age').val();
+  let planet = $("input:radio[name=planet]:checked").val();
+  console.log(age);
+  console.log(planet);
+})

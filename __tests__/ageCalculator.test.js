aaf6db5c9bd92460ae33eec8250ac27b38c98d86
FrankTimmons <franktimmonspdx@gmail.com>
2022-06-24 09:13:59 -0700

    Add red phase of first test.

--- __tests__/ageCalculator.test.js
+++ __tests__/ageCalculator.test.js
@@ -4,6 +4,10 @@ describe('User', () => {
   let newUser;
 
   beforeEach(() =>{
-    newUser = new User("22");
+    newUser = new User(22);
+  });
+
+  test('Should make a new user object with an age property', () => {
+    expect(newUser.age).toEqual(22);
   });
 });

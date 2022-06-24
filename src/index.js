57e15346b6cdcb50471e06af4f6aea31bbf52ec5
FrankTimmons <franktimmonspdx@gmail.com>
2022-06-24 15:53:11 -0700

    Add a function to show output in the html.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { User } from './js/ageCalculator.js'
+import { User } from './js/ageCalculator.js';
 
 $('form#age-calc-form').submit(function(event){
   event.preventDefault();
   let age = parseInt($('#age').val());
+  let earthLifeExpectancy = parseInt($('#life-expect').val());
   let planet = $("input:radio[name=planet]:checked").val();
   let newUser = new User(age);
-  planetChecker(newUser, planet);
+  planetChecker(newUser, planet, earthLifeExpectancy);
+  $('#output').show();
+  $('#planet').text(planet);
+  $('#planet-age').text(newUser.planetAge);
 });
 
-const planetChecker = (user, planet) => {
+const planetChecker = (user, planet, lifeExpectancy) => {
   if (planet == "Mercury"){
-    user.mercuryAgeCalc();
+    user.mercuryAgeCalc(lifeExpectancy);
   }else if (planet == "Venus"){
-    user.venusAgeCalc();
+    user.venusAgeCalc(lifeExpectancy);
   }else if (planet == "Mars"){
-    user.marsAgeCalc();
+    user.marsAgeCalc(lifeExpectancy);
   }else {
-    user.jupiterAgeCalc();
+    user.jupiterAgeCalc(lifeExpectancy);
   }
-}
+};

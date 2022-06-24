051931412d8f10333311ad8ca1063a6177523ca4
FrankTimmons <franktimmonspdx@gmail.com>
2022-06-24 15:33:14 -0700

    Add a function to find out which planet is checked, then call the correct age calculator.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { User } from './js/ageCalculator.js'
+import { User } from './js/ageCalculator.js'
 
 $('form#age-calc-form').submit(function(event){
   event.preventDefault();
-  let age = $('#age').val();
+  let age = parseInt($('#age').val());
   let planet = $("input:radio[name=planet]:checked").val();
-  console.log(age);
-  console.log(planet);
-})
+  let newUser = new User(age);
+  planetChecker(newUser, planet);
+});
+
+const planetChecker = (user, planet) => {
+  if (planet == "Mercury"){
+    user.mercuryAgeCalc();
+  }else if (planet == "Venus"){
+    user.venusAgeCalc();
+  }else if (planet == "Mars"){
+    user.marsAgeCalc();
+  }else {
+    user.jupiterAgeCalc();
+  }
+}

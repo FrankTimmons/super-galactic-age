a7be5736cd59945d81b0788817d21c1641ca6040
FrankTimmons <franktimmonspdx@gmail.com>
2022-06-24 09:46:11 -0700

    Pass third test.

--- src/ageCalculator.js
+++ src/ageCalculator.js
@@ -1,8 +1,14 @@
 export class User {
+
   constructor(age){
     this.age = age;
   }
+
   mercuryAgeCalc(){
     this.mercuryAge = this.age / .24;
+  }
+  
+  venusAgeCalc(){
+    this.venusAge = this.age / .62;
   }
 }
